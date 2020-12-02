9c77b4c518cb2e859b36859ed896c271e5b45295
James Morgan O <MorganJay@users.noreply.github.com>
2020-12-02 16:15:15 +0100

    fixed int.maxvalue

--- AlgorithmChallenge2/Program.cs
+++ AlgorithmChallenge2/Program.cs
@@ -9,12 +9,11 @@ namespace AlgorithmChallenge2
     {
         private static void Main(string[] args)
         {
-            const int end = int.MaxValue;
             ulong sum = 0;
             Console.WriteLine("This program is to compute the sum of all positive prime numbers that are in the C# int data type, \nthat is from 0 - {0:###,###}.",
                 int.MaxValue);
             Console.WriteLine("\nPlease wait while I'm calculating. \nThis might take a while....");
-            for (var i = 0; i < end; i++)
+            for (var i = 0; i < int.MaxValue; i++)
             {
                 for (var j = i - 1; j > 0; j--)
                 {
